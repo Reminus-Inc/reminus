3fa032855cf42c3c7cd279a615b3b2d1448210b3
sumiren <sumiren.and.t@gmail.com>
2025-05-17 16:39:50 +0900

    fix

--- src/app/_components/cto-partner/index.tsx
+++ src/app/_components/cto-partner/index.tsx
@@ -2,17 +2,12 @@
 
 import { CtoPartnerHero } from "./cto-partner-hero";
 import { EngineeringElements } from "../engineering-elements";
-import { ServicePlans } from "../service-plans";
-import { PhaseSupport } from "../phase-support";
-import { EngagementStyle } from "../engagement-style";
-import { CtoPartnerCta } from "../cto-partner-cta";
 
 export function CtoPartner() {
   return (
     <div className="flex flex-col">
       <main className="">
         <CtoPartnerHero />
-        <EngineeringElements />
         {/*<ServicePlans />
         <PhaseSupport />
         <EngagementStyle />
