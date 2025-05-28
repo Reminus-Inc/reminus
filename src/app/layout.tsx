cb379e81d97d9ed2537ccea487deec195b4b50e9
sumiren <sumiren.and.t@gmail.com>
2025-05-28 16:02:08 +0900

    chore: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,12 +70,23 @@ export default async function RootLayout({
         <Toaster />
         {/* XPixelPageView はベタ打ちPixel IDでも動作するように更新 */}
         <XPixelPageView />
+        {!!process.env.GA_ID && <GoogleAnalytics gaId={process.env.GA_ID} />}
+        {/* Twitter/X Pixel - 初期化してから読み込み */}
+        <Script
+          id="x-pixel-init"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(e,t,n,s,u,a){
+                e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);},
+                s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+                a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))
+              }(window,document,'script');
+              twq('config','pto6l');
+            `,
+          }}
+        />
       </body>
-      {!!process.env.GA_ID && <GoogleAnalytics gaId={process.env.GA_ID} />}
-      {/* Twitter/X Pixel - ベタ打ちで実装 */}
-      <Script id="x-pixel" strategy="beforeInteractive">
-        {`!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);};s.version='1.1';s.queue=[];u=t.createElement(n);u.async=!0;u.src='https://static.ads-twitter.com/uwt.js';a=t.getElementsByTagName(n)[0];a.parentNode.insertBefore(u,a))}(window,document,'script');twq('config','pto6l');`}
-      </Script>
     </html>
   );
 }
