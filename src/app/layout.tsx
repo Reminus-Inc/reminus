f64a1f50f7387d9ae828a9fde9f3f1b5d06632be
sumiren <sumiren.and.t@gmail.com>
2025-05-28 14:34:07 +0900

    chore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,7 +73,7 @@ export default async function RootLayout({
       {!!process.env.GA_ID && <GoogleAnalytics gaId={process.env.GA_ID} />}
       {!!process.env.X_PIXEL_ID && (
         <Script id="x-pixel" strategy="beforeInteractive">
-          {`!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);};s.version='1.1';s.queue=[];u=t.createElement(n);u.async=!0;u.src='//static.ads-twitter.com/uwt.js';a=t.getElementsByTagName(n)[0];a.parentNode.insertBefore(u,a))}(window,document,'script');twq('config','${process.env.X_PIXEL_ID}');twq('track','PageView');`}
+          {`!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);};s.version='1.1';s.queue=[];u=t.createElement(n);u.async=!0;u.src='https://static.ads-twitter.com/uwt.js';a=t.getElementsByTagName(n)[0];a.parentNode.insertBefore(u,a))}(window,document,'script');twq('config','${process.env.X_PIXEL_ID}');`}
         </Script>
       )}
     </html>
