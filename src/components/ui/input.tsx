72a9d2aef7bf54a8cc112a07f1d86ce423913711
sumiren <sumiren.and.t@gmail.com>
2025-08-30 12:50:21 +0900

    style: input text 16px

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
       <input
         type={type}
         className={cn(
-          "block w-full rounded border border-input bg-transparent px-4 py-3 text-sm tracking-wide text-gray-800 transition-colors placeholder:text-gray-400 focus-visible:border-indigo-500 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-60",
+          "block w-full rounded border border-input bg-transparent px-4 py-3 text-base tracking-wide text-gray-800 transition-colors placeholder:text-gray-400 focus-visible:border-indigo-500 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-60",
           className
         )}
         ref={ref}
