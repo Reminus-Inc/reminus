453ace02048e417cc5b895144927fb8e22a0f040
sumiren <sumiren.and.t@gmail.com>
2025-09-05 03:01:01 +0900

    fix: form

---
+++ src/app/cto-team/second-download-form.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { DownloadForm } from "../_components/ui/download-form";
+import { DOCUMENT_TYPE } from "../constants";
+
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    immedio: any;
+  }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const immedio: any;
+}
+
+export function SecondDownloadForm() {
+  return (
+    <DownloadForm documentType={DOCUMENT_TYPE.CTO_UNIT} beforeThanks={(formValues) => {
+      immedio.submitFormData({
+        additionalData: {
+            email: formValues.email,
+            person_name: formValues.name,
+            phone_number: formValues.phone,
+            company_name: formValues.company,
+        },
+      });
+    }} />
+  );
+}
