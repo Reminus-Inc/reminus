86b82a020bb1f16290b4d0353fa2d9872823171c
sumiren <sumiren.and.t@gmail.com>
2026-01-23 00:21:15 +0900

    fix: font

--- src/app/_components/layout/header.tsx
+++ src/app/_components/layout/header.tsx
@@ -27,7 +27,7 @@ export function Header({
   };
 
   return (
-    <header className="sticky left-0 top-0 z-10 flex h-[60px] items-center bg-white md:h-[80px]">
+    <header className="sticky left-0 top-0 z-10 flex h-[60px] items-center bg-white md:h-[80px] font-sans">
       <div className="mx-auto w-full max-w-[1360px] px-4 sm:px-6">
         <div className="flex h-full items-center justify-between">
           <div className="flex items-center">
