6843ce5c2a1ca14f281e61cc1a151f66586050b4
sumiren <sumiren.and.t@gmail.com>
2026-01-25 05:12:14 +0900

    chore: remove layout shift

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import { Footer } from "./_components/layout/footer";
 import { DownloadDialogProvider } from "@/app/_components/ui/download-dialog-context";
 import { DownloadDialog } from "@/app/_components/ui/download-dialog";
 import { DownloadDialogCloser } from "@/app/_components/layout/download-dialog-closer";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import { PersistUtm } from "./_components/PersistUtm"
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
@@ -14,7 +13,6 @@ const notoSansJP = Noto_Sans_JP({
   subsets: ["latin"],
   variable: "--font-noto-sans-jp",
   preload: false,
-  display: "swap",
 });
 
 export const metadata = {
