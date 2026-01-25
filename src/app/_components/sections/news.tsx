3ff0354a13b10f21a57a2bbf765f88e7688a7794
RikuOta <otarikuweb@gmail.com>
2026-01-25 17:42:16 +0900

    chrome の最新バージョンでスクロールが効かない不具合を修正

--- src/app/_components/sections/news.tsx
+++ src/app/_components/sections/news.tsx
@@ -16,7 +16,10 @@ type ArticleType = (typeof ARTICLE_TYPE)[keyof typeof ARTICLE_TYPE];
 
 export function News({ className }: { className?: string }) {
   return (
-    <section id="news" className={cn("py-24 sm:py-32 font-sans content-auto", className)}>
+    <section
+      id="news"
+      className={cn("overflow-x-hidden py-24 sm:py-32", className)}
+    >
       <div className="mx-auto w-[82%] max-w-[1200px] md:w-[86%]">
         <SectionHeader
           label="News"
